363d39de7751d88d5606f214c4956ff41b4bac06
Loth'Tikar <>
2018-04-10 14:24:07 +0000

    added basic stuff for using gpio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+extern crate libc;
+
+use std::os::unix::io::AsRawFd;
+
 const MAX_SPEED: f64 = 1.0;
 const MIN_SPEED: f64 = -1.0;
 
@@ -81,4 +85,35 @@ fn main() {
     left_speed.expect("No value provided for left speed");
     right_speed.expect("No value provided for right speed");
     time.expect("No value provided for time");
+
+    let mem_file = std::fs::OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open("/dev/gpiomem")
+        .expect("failed to open /dev/gpiomem");
+
+    unsafe {
+        let mem = libc::mmap(
+            0 as *mut libc::c_void,
+            0x00B0,
+            libc::PROT_READ | libc::PROT_WRITE,
+            libc::MAP_SHARED,
+            mem_file.as_raw_fd(),
+            0,
+        ) as *mut u32;
+
+        let pin_mode_1 = mem.offset(1);
+        *pin_mode_1 = 1 << 8 | 1 << 11;
+
+        let pin_set_1 = mem.offset(7);
+        let pin_clear_1 = mem.offset(10);
+
+        loop {
+            *pin_set_1 = 1 << 26;
+            std::thread::sleep(std::time::Duration::from_millis(500));
+
+            *pin_clear_1 = 1 << 26;
+            std::thread::sleep(std::time::Duration::from_millis(500));
+        }
+    }
 }
